2289598b8f9c68bd67515f285cd173703dfe0da9
Nandlal Lambole <nandlal.lambole@gmail.com>
2021-01-03 18:19:44 +0530

    -

---
+++ OpenGL/Demo/Assignment/Windows/FixedFunctionPipeline/49_Lights/04_Three_Rotating_Lights_on_Sphere/04_Three_Rotating_Lights_on_Sphere.cpp
@@ -0,0 +1,495 @@
+//Headers
+#include <windows.h>
+#include <stdio.h>
+#include "NRL.h"
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <math.h>
+
+#pragma comment(lib, "glu32")
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+#define Pi 3.14159f
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbFullscreen = false;
+bool gbActiveWindow = false;
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+FILE* gpFile = NULL;
+
+int Width;
+int Height;
+
+//************* Light / Texure*************
+bool bLight = true;
+
+//Light Arrays
+GLfloat LightAmbient_Zero[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+GLfloat LightDiffuse_Zero[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+GLfloat LightSpecular_Zero[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+GLfloat LightPosition_Zero[] = { 0.0f, 0.0f, 0.0f, 1.0f }; //Positional Light
+
+GLfloat LightAmbient_One[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+GLfloat LightDiffuse_One[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+GLfloat LightSpecular_One[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+GLfloat LightPosition_One[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+GLfloat LightAmbient_Two[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+GLfloat LightDiffuse_Two[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+GLfloat LightSpecular_Two[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+GLfloat LightPosition_Two[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+//Material Arrays
+GLfloat MaterialAmbient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+GLfloat MaterialDiffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat MaterialSpecular[] = { 1.0f, 1.0f , 1.0f, 1.0f };
+GLfloat MaterialShininess = 50.0f;
+
+GLfloat LightAngle0 = 0.0f;
+GLfloat LightAngle1 = 0.0f;
+GLfloat LightAngle2 = 0.0f;
+
+
+//Transformation
+GLfloat Angle = 45.0f;
+GLfloat fCounter = 0.05f;
+
+GLfloat X = 1.0f;
+GLfloat Y = 1.0f;
+GLfloat Z = 1.0f;
+
+//Shape
+GLUquadric* quadric = NULL;
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdlie, int iCmdShow)
+{
+	//Function Declaration
+	void Display(void);
+	void Initialize(void);
+	//void Display(void);
+
+	//Varable Declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("FullScreen Window");
+	bool bDone = false;
+
+	if (fopen_s(&gpFile, "NRLLog.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Cannot Open Desired File"), ERROR, MB_OK);
+		exit(0);
+	}
+
+	// code
+	// Initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	// Register Abouve Code
+	RegisterClassEx(&wndclass);
+
+	Width = (GetSystemMetrics(SM_CXSCREEN) / 2 - WIN_WIDTH / 2);
+	Height = (GetSystemMetrics(SM_CYSCREEN) / 2 - WIN_HEIGHT / 2);
+
+	// Create Window
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW, szAppName, TEXT("Three Rotating Lights On Sphere : Nandlal Lambole"), WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE, Width, Height, WIN_WIDTH, WIN_HEIGHT, NULL, NULL, hInstance, NULL);
+	ghwnd = hwnd;
+
+	Initialize(); //Call           
+
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	//GAME Loop..
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bDone = true;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				//Here You Should Call Update Function For OpenGL Rendering
+
+				//Here You Should Call Display Function For OpenGL Rendering
+				Display(); //Call Display
+			}
+
+		}
+	}
+
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	//Function Prototype
+	void ToggleFullScreen(void);
+	void Resize(int Width, int Height);
+	void UnInitialize(void);
+
+
+	// Code
+	switch (iMsg)
+	{
+
+	case WM_CREATE:
+		fprintf(gpFile, "India Is My Country\n\n");
+		break;
+
+	case WM_SETFOCUS:
+		gbActiveWindow = true;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = false;
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_SIZE:
+		Resize(LOWORD(lParam), HIWORD(lParam));
+
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+
+		case VK_ESCAPE:
+			DestroyWindow(hwnd); //Win32 API
+			break;
+
+		case 0x46:
+			//case 0x66:
+			ToggleFullScreen(); //Call
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'L':
+		case 'l':
+			if (bLight == false)
+			{
+				glEnable(GL_LIGHTING);
+				bLight = true;
+			}
+			else
+			{
+				glDisable(GL_LIGHTING);
+				bLight = false;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd); //Win32 API
+		break;
+
+	case WM_DESTROY:
+		UnInitialize(); //Call
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen(void)
+{
+	//Local Variable Declarations
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+	//Code
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~WS_OVERLAPPEDWINDOW));
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, (mi.rcMonitor.right - mi.rcMonitor.left), (mi.rcMonitor.bottom - mi.rcMonitor.top), SWP_NOZORDER | SWP_FRAMECHANGED);
+
+			}
+		}
+		ShowCursor(FALSE);
+		gbFullscreen = true;
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(TRUE);
+		gbFullscreen = false;
+
+	}
+}
+
+void Initialize(void)
+{
+	//Function Declaration
+	void Resize(int, int);
+
+	// Variable Declaration
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	//Code
+	Resize(WIN_WIDTH, WIN_HEIGHT); //WarmUp Call To Resize
+
+	ghdc = GetDC(ghwnd);
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		fprintf(gpFile, "ChoosePixel Format Function Failed..!");
+		DestroyWindow(ghwnd);
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		fprintf(gpFile, "SetPixel Format Function Failed..!");
+		DestroyWindow(ghwnd);
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL)
+	{
+		fprintf(gpFile, "wglCreateContext Function Failed..!");
+		DestroyWindow(ghwnd);
+	}
+
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFile, "wglMakeCurrent Function Failed..!");
+		DestroyWindow(ghwnd);
+	}
+
+	//************************************_DEPTH_*********************************************
+	//Depth Function
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	//Accessory Function
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); //Distortion Correction
+	//****************************************************************************************
+
+	//SetClearColour
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//_________________________________________*LIGHT*_________________________________________
+	//Light 
+	glLightfv(GL_LIGHT0, GL_AMBIENT, LightAmbient_Zero); //f - float. v- Vector
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, LightDiffuse_Zero);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, LightSpecular_Zero);
+	glEnable(GL_LIGHT0); //Enable Light
+
+	glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient_One); //f - float. v- Vector
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse_One);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, LightSpecular_One);
+	glEnable(GL_LIGHT1); //Enable Light
+
+	glLightfv(GL_LIGHT2, GL_AMBIENT, LightAmbient_Two); //f - float. v- Vector
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, LightDiffuse_Two);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, LightSpecular_Two);
+	glEnable(GL_LIGHT2); //Enable Light
+
+	//Material Initialization
+	glMaterialfv(GL_FRONT, GL_AMBIENT, MaterialAmbient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, MaterialDiffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, MaterialSpecular);
+	glMaterialf(GL_FRONT, GL_SHININESS, MaterialShininess);
+
+	//__________________________________________________________________________________________
+
+	//WarmUp Resize Call
+	Resize(WIN_WIDTH, WIN_HEIGHT);
+
+}
+
+void Resize(int width, int height)
+{
+	//Code
+	if (height == 0)
+	{
+		height = 1;
+	}
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+void Display(void)
+{
+	//Function Declarations
+	void Sphere(void);
+	void Update(void);
+
+	//Code
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glPushMatrix();
+	gluLookAt(0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	glPushMatrix();
+	glRotatef(LightAngle0, 1.0f, 0.0f, 0.0f);
+	LightPosition_Zero[1] = LightAngle0;
+	glLightfv(GL_LIGHT0, GL_POSITION, LightPosition_Zero);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(LightAngle1, 0.0f, 1.0f, 0.0f);
+	LightPosition_One[0] = LightAngle1;
+	glLightfv(GL_LIGHT1, GL_POSITION, LightPosition_One);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(LightAngle2, 0.0f, 0.0f, 1.0f);
+	LightPosition_Two[1] = LightAngle2;
+	glLightfv(GL_LIGHT2, GL_POSITION, LightPosition_Two);
+	glPopMatrix();
+	
+	glTranslatef(0.0f, 0.0f, -0.1f); //Hybrid Translation with glLookAt()
+	Sphere();
+	Update();
+	glPopMatrix();
+
+	
+	SwapBuffers(ghdc); //Native API for Windowing
+}
+
+void Sphere(void)
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	quadric = gluNewQuadric();
+	//Sphere
+	gluSphere(quadric, 0.2f, 30, 30); //Normals are calculated by gluSphere itself;
+}
+
+void Update(void)
+{
+	LightAngle0 += 1.0f;
+	if (LightAngle0 >= 360.0f)
+	{
+		LightAngle0 = 0.0f;
+	}
+
+	LightAngle1 += 1.0f;
+	if (LightAngle1 >= 360.0f)
+	{
+		LightAngle1 = 0.0f;
+	}
+
+	LightAngle2 += 1.0f;
+	if (LightAngle2 >= 360.0f)
+	{
+		LightAngle2 = 0.0f;
+	}
+
+}
+
+
+
+void UnInitialize(void)
+{
+	//Code
+	if (gbFullscreen == true)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (gpFile)
+	{
+		fclose(gpFile);
+		gpFile = NULL;
+
+	}
+
+	if (quadric)
+	{
+		gluDeleteQuadric(quadric);
+		quadric = NULL;
+	}
+}
